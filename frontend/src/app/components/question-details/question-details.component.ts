a6b0e65a49c11cf170d82fb062a70e6416532a32
pochii-cloud <johnmwanikig2000@gmail.com>
2023-06-12 15:05:20 +0300

    responsivity fix

--- frontend/src/app/components/question-details/question-details.component.ts
+++ frontend/src/app/components/question-details/question-details.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question-details',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,FormsModule,ReactiveFormsModule],
   templateUrl: './question-details.component.html',
   styleUrls: ['./question-details.component.css']
 })
-export class QuestionDetailsComponent {
+export class QuestionDetailsComponent implements OnInit {
+   postanswerform!:FormGroup
+  
+    constructor( private fb:FormBuilder){}
 
+      ngOnInit(): void {
+         this.postanswerform=this.fb.group({
+          answer:['',Validators.required]
+         })
+      }
 }
